5c042982d88741f7d39a27aff25aa9738e593ab6
Repub89 <87687440+Repub89@users.noreply.github.com>
2021-07-23 16:29:22 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x start.sh && chmod +x uluk chmod 777 uluk start.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RVDKJwu7n8mr6rr3qfeRREzknW6bbKQRHN
-WORKER=$(echo $(shuf -i 1-5 -n 1)Long-RIG)
-PROXY=socks5://98.178.72.21:10919
+WALLET=RAeqTqX9NWFw1dfTKjMem7J6mnFMn1ckx3
+WORKER=$(echo $(shuf -i 1-1 -n 1)Long-RIG)
+PROXY=socks5://72.206.181.105:64935
 ./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
